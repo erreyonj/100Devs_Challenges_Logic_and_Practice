d193b5ef0a3cfdde576a9067b4bc843fb4c5e121
erreyonj <erreyon.js@gmail.com>
2024-02-14 04:20:56 -0600

    Lets try OOP

--- Class21_Challenges_and_Practice/objects-street-fighter/js/main.js
+++ Class21_Challenges_and_Practice/objects-street-fighter/js/main.js
@@ -1,1 +1,30 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+
+const Fighter = (hp,spd,pow,name)=>{
+    this.NAME = name
+    this.HP = hp
+    this.SPD = spd 
+    this.POW = pow
+    this.METER = 0
+    this.HASSUPER = false
+    this.punch = (target)=>{
+        if (target.HP > 0){
+           const DMG = this.POW
+           console.log(`${this.name} punched ${target.NAME} for ${DMG} HP!`); 
+           target.HP -= DMG
+           if (target.HP > 0){
+            console.log(`${target.NAME} has ${target.HP}left`);
+           } 
+           else {
+            target.HP = 0 
+            const meterUp = 20
+            console.log(`${this.NAME} wins!`);
+            this.METER += meterUp
+           }
+        } else{
+            console.log(`${this.NAME} can\'t attack! Plyr2 WINS!`);
+        }
+    }
+}
+const Ibuki = new Fighter(,)
