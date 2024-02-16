e43c36c4366dbbb1229887e1adb8d148dab28fce
erreyonj <erreyon.js@gmail.com>
2024-02-15 22:04:18 -0600

    Class 22 Work

--- Class22_Challenges_and_Practice/objects-lost-galaxy/js/main.js
+++ Class22_Challenges_and_Practice/objects-lost-galaxy/js/main.js
@@ -1,1 +1,54 @@
 //Create a mouse object that has four properties and three methods
+
+const poke = {
+    name: 'Pikachu',
+    type: 'electric',
+    color: 'yellow',
+    attacks: 1,
+    power: 'thunderbolt',
+
+    attack: function(){
+        console.log(`${this.name} use ${this.power}!!`)
+    },
+    pokeColor: function(){
+        console.log(`${this.name} is ${this.color}`);
+    },
+    pokeType: function(){
+        console.log(`${this.name} is an ${this.type} type`);
+    }
+}
+
+poke.attack()
+poke.pokeColor()
+poke.pokeType()
+
+
+function Pokemon(name,type,color,power){
+    this.name = name
+    this.type = type
+    this.color = color
+    this.attacks = 1
+    this.power = power
+    this.attack = function(){
+        console.log(`${this.name} use ${this.power}!!`)
+    },
+    this.pokeColor = function(){
+        console.log(`${this.name} is ${this.color}`)
+    },
+    this.pokeType = function(){
+        console.log(`${this.name} is ${this.type} type`)
+    }
+}
+
+const salamence = new Pokemon('Salamence','Dragon','Blue and Red','Dragon Claw')
+
+salamence.attack()
+salamence.pokeColor()
+salamence.pokeType()
+
+salamence.lv = 100
+
+Pokemon.prototype.exp = 5000
+
+console.log(salamence);
+console.log(salamence.exp);
--- Class22_Challenges_and_Practice/objects-street-fighter/js/main.js
+++ Class22_Challenges_and_Practice/objects-street-fighter/js/main.js
@@ -1,1 +1,23 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(name,hp,atk,spd){
+    this.name = name
+    this.hp = hp
+    this.atk = atk
+    this.spd = spd
+    this.lightPunch = function(target){
+        console.log(`${this.name} hit ${target.name} and they lost ${this.atk/2} hp!`)
+    }
+    this.punch = function(target){
+        console.log(`${this.name} hit ${target.name} and they lost ${this.atk} hp!`)
+    }
+    this.heavyPunch = function(target){
+        console.log(`${this.name} hit ${target.name} and they lost ${this.atk*2} hp!`)
+    }
+}
+
+const ryu = new StreetFighter('Ryu',100,20,100)
+const ken = new StreetFighter('Ken',100,25,100)
+
+ryu.lightPunch(ken)
+ken.heavyPunch(ryu)
