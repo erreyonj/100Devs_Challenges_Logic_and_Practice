6f96af1b525677d217dc5ef343511095b5dfd173
erreyonj <erreyon.js@gmail.com>
2024-02-20 16:11:35 -0600

    Triangular Sort Challenge

--- general_work.js
+++ general_work.js
@@ -240,7 +240,7 @@ var twoSum2 = function(nums, target) {
 ////////////////
 // 2/19/24
 ////////////////
-
+//SOLVED
 
 // In this exercise, you will develop a function named decode(message_file). This function should read an encoded message from a .txt file and return its decoded version as a string.
 
@@ -266,36 +266,451 @@ var twoSum2 = function(nums, target) {
 // 6: computers
 // and your function should return the string "I love computers".
 
+// simple test
 let message = ['3 love','6 computers','2 dogs','4 cats','1 I','5 you']
 
+// true test case
+let bigMessage = [
+    "244 land",
+    "203 sun",
+    "190 too",
+    "49 huge",
+    "291 dont",
+    "296 such",
+    "202 noun",
+    "114 student",
+    "181 brown",
+    "164 complete",
+    "157 play",
+    "87 cook",
+    "183 yard",
+    "236 clock",
+    "161 would",
+    "160 plain",
+    "91 excite",
+    "257 fire",
+    "103 wish",
+    "255 cool",
+    "206 child",
+    "109 past",
+    "129 colony",
+    "196 oil",
+    "128 dog",
+    "189 back",
+    "24 money",
+    "165 kind",
+    "40 open",
+    "141 finger",
+    "6 touch",
+    "242 are",
+    "231 dad",
+    "122 am",
+    "74 modern",
+    "169 meant",
+    "258 ocean",
+    "15 pitch",
+    "233 suit",
+    "251 town",
+    "11 east",
+    "69 over",
+    "256 group",
+    "78 good",
+    "286 kind",
+    "14 down",
+    "51 band",
+    "96 especially",
+    "192 organ",
+    "112 of",
+    "243 fire",
+    "140 out",
+    "105 area",
+    "107 touch",
+    "32 happen",
+    "45 sat",
+    "23 electric",
+    "46 wrote",
+    "283 buy",
+    "241 lot",
+    "226 stop",
+    "77 corn",
+    "168 where",
+    "297 check",
+    "16 live",
+    "2 best",
+    "237 hold",
+    "250 cause",
+    "101 grand",
+    "100 present",
+    "130 indicate",
+    "8 counter",
+    "21 we",
+    "66 like",
+    "36 visit",
+    "240 state",
+    "147 morning",
+    "228 true",
+    "93 are",
+    "184 ball",
+    "217 history",
+    "207 seat",
+    "52 rain",
+    "135 less",
+    "268 glass",
+    "239 tone",
+    "289 song",
+    "288 fair",
+    "55 element",
+    "272 speed",
+    "299 produce",
+    "152 quotient",
+    "170 sand",
+    "172 begin",
+    "115 moment",
+    "198 offer",
+    "163 probable",
+    "195 all",
+    "90 necessary",
+    "30 post",
+    "89 cent",
+    "260 happen",
+    "106 speech",
+    "259 object",
+    "248 silver",
+    "148 third",
+    "273 crease",
+    "26 wait",
+    "188 triangle",
+    "277 idea",
+    "187 clothe",
+    "178 young",
+    "33 discuss",
+    "191 field",
+    "222 company",
+    "179 capital",
+    "204 compare",
+    "199 chart",
+    "35 possible",
+    "31 written",
+    "97 remember",
+    "276 mile",
+    "118 cold",
+    "102 lady",
+    "3 felt",
+    "290 against",
+    "232 skin",
+    "76 prepare",
+    "79 he",
+    "274 card",
+    "123 organ",
+    "98 object",
+    "201 our",
+    "279 major",
+    "41 discuss",
+    "70 system",
+    "205 hole",
+    "92 above",
+    "17 they",
+    "65 produce",
+    "95 straight",
+    "4 level",
+    "263 though",
+    "82 modern",
+    "85 dry",
+    "27 bought",
+    "43 milk",
+    "252 make",
+    "133 show",
+    "227 middle",
+    "42 center",
+    "83 blood",
+    "54 speak",
+    "159 prove",
+    "80 select",
+    "247 power",
+    "143 come",
+    "104 brown",
+    "208 experiment",
+    "13 strong",
+    "116 hurry",
+    "19 touch",
+    "224 reach",
+    "84 case",
+    "44 beat",
+    "173 over",
+    "175 dry",
+    "47 hill",
+    "67 company",
+    "120 opposite",
+    "280 work",
+    "167 field",
+    "246 felt",
+    "139 prepare",
+    "99 now",
+    "262 his",
+    "81 stay",
+    "12 toward",
+    "298 observe",
+    "253 time",
+    "154 stop",
+    "71 possible",
+    "225 card",
+    "38 prepare",
+    "113 current",
+    "126 compare",
+    "156 neighbor",
+    "197 thus",
+    "278 include",
+    "177 copy",
+    "48 bit",
+    "166 stead",
+    "200 does",
+    "282 general",
+    "144 solve",
+    "110 glad",
+    "275 duck",
+    "281 offer",
+    "219 happen",
+    "56 ball",
+    "223 bread",
+    "287 like",
+    "171 machine",
+    "149 come",
+    "294 any",
+    "22 band",
+    "292 it",
+    "134 section",
+    "94 close",
+    "50 heavy",
+    "53 produce",
+    "25 got",
+    "213 possible",
+    "64 insect",
+    "125 way",
+    "261 before",
+    "285 men",
+    "295 bird",
+    "59 ease",
+    "284 trade",
+    "249 winter",
+    "137 am",
+    "150 repeat",
+    "216 first",
+    "185 to",
+    "138 each",
+    "61 guide",
+    "267 column",
+    "72 single",
+    "235 remember",
+    "60 wild",
+    "230 major",
+    "63 coast",
+    "229 class",
+    "220 done",
+    "271 jump",
+    "57 sister",
+    "221 feel",
+    "180 check",
+    "39 fire",
+    "7 nine",
+    "212 indicate",
+    "153 parent",
+    "146 whole",
+    "211 her",
+    "151 the",
+    "10 temperature",
+    "29 design",
+    "270 big",
+    "186 skill",
+    "62 friend",
+    "5 hit",
+    "234 wait",
+    "34 instant",
+    "264 blow",
+    "58 about",
+    "124 chick",
+    "215 answer",
+    "37 man",
+    "158 material",
+    "155 current",
+    "136 think",
+    "142 print",
+    "193 nor",
+    "121 better",
+    "20 example",
+    "68 people",
+    "9 drink",
+    "88 gun",
+    "218 together",
+    "176 cost",
+    "145 require",
+    "245 or",
+    "111 people",
+    "75 planet",
+    "28 ease",
+    "238 ready",
+    "269 enough",
+    "254 sugar",
+    "214 deal",
+    "182 with",
+    "108 us",
+    "209 share",
+    "119 office",
+    "127 protect",
+    "265 low",
+    "210 thus",
+    "1 farm",
+    "174 oxygen",
+    "73 fire",
+    "86 force",
+    "293 select",
+    "117 paragraph",
+    "266 always",
+    "132 poem",
+    "18 chick",
+    "131 planet",
+    "194 fact",
+    "162 moment",
+    "300 term"
+]
 
 
-function Decode(message_file){
-    //triangle function that could be used to sort any numbered array into pyramid
-    function pyramidFunc(num){
-        return (num*(num+1))/2
+// This train of thought gets the contents of txt file, but we can prove Decode works without this feature.
+// async function logText(){
+//     const response = await fetch('/244 land.txt')
+//     const theBody = await response.text()
+//     console.log(theBody);
+//     const theLine = theBody.split('\r\n')
+//     console.log(theLine);
+//     return theLine
+// }
+
+// logText()
+// let message2 = logText()
+// console.log(message2);
+
+
+
+
+
+/////
+//Possibly useful func
+///////
+// function pyramidFunc(num){
+//     return (num*(num+1))/2
+// }
+
+
+let message3 = ['4 mizu','99 oto','3 this','5 kaminari','2 mizu','1 think','6 should','8 oto','7 kaminari','10 work','11 huh?', '12 poke', '13 bleep', '148 cash', '15 I think']
+
+function decode(arr){
+    let decodedArr = []
+
+    //need function that sorts array by number appearing in element
+    // let primeSort = arr.sort((a,b)=>a.localeCompare(b))
+    // console.log(primeSort);
+    function trueSort(words){
+        let sorted = [];
+        words.forEach((word)=>{
+            let pos = (word)=>{
+                for(let i = 0;i < word.length;i++){
+                    if(!isNaN(word.charAt(i)) && !(word.charAt(i) === '')){
+                        if(!isNaN(word.charAt(i+1)) && !(word.charAt(i+1) === '')){
+                            if(!isNaN(word.charAt(i+2)) && !(word.charAt(i+2) === '')){
+                                return `${word[0]}${word[1]}${word[2]}`
+                            } else return `${word[0]}${word[1]}`
+                        } else return `${word[0]}`
+                    } else return ''
+                }
+            }
+            sorted[pos(word)-1] = word
+        }) 
+        // console.log(sorted);
+        return sorted
+        // return sorted.toString().split(',').join(' ').replaceAll(/\d/g,'')
     }
 
-    //need function that sorts a new array by number appearing in element
-    let primeSort = message_file.sort((a,b)=>a.localeCompare(b))
-    
+    let primeSort = trueSort(arr)
+    console.log(primeSort);
 
-    //function that ejects an element from new array to new array then increases the number of ejected elements until all elements ejected
-    let peak1 = primeSort.slice(0,1)
-    let peak2 = primeSort.slice(1,3)
-    let peak3 = primeSort.slice(3,6)
+    //loop that ejects 1 element from primeSorted to decodedArr then increases the number of ejected elements until all elements ejected, also sanitizes numbers from each element
+
+    let j = 1
+    while(primeSort[0]){
+        // let length = primeSort.length
+        // console.log(length);
+        // console.log(`${j}`);
+        // let j = 1
+        decodedArr.push(primeSort.splice(0,j).pop().slice(2))
+        // decodedArr.push(primeSort.splice(0,1))
+        // console.log(peak1);
+        // console.log(primeSort)
+        j++
+    }
+
+    // let peak1 = primeSort.slice(0,1)
+    // let peak2 = primeSort.slice(1,3)
+    // let peak3 = primeSort.slice(3,6)
+
     
 
     //func that puts last element in each ejected array into new array with no numbers
-    let peak1pop = peak1.pop().slice(2)
-    let peak2pop = peak2.pop().slice(2)
-    let peak3pop = peak3.pop().slice(2)
+    // let peak1pop = peak1.pop().slice(2)
+    // let peak2pop = peak2.pop().slice(2)
+    // let peak3pop = peak3.pop().slice(2)
 
     //print result
-    console.log(`${peak1pop} ${peak2pop} ${peak3pop}`)
+    // console.log(decodedArr.join(' '));
+    const sani = decodedArr.join(' ')
+    return sani.split(',').join(' ').replaceAll(/\d/g,'')
+}
+// Decode(message)
+
+
+
+
+/////////////
+//CLEANED ALL UNECESSARY PSUEDO CODE, LEFT COMMMENTS 
+//////////////
+
+
+function decode2(arr){
+    let decodedArr = []
+    //need function that sorts input array by the number appearing in element
+    function trueSort(words){
+        let sorted = [];
+        words.forEach((word)=>{
+            let pos = (word)=>{
+                for(let i = 0;i < word.length;i++){
+                    //checks if first char is num
+                    if(!isNaN(word.charAt(i)) && !(word.charAt(i) === '')){
+                        //checks if second char is num
+                        if(!isNaN(word.charAt(i+1)) && !(word.charAt(i+1) === '')){
+                            //checks if third char is num(only allows input array up to 999 elements in theory)
+                            if(!isNaN(word.charAt(i+2)) && !(word.charAt(i+2) === '')){
+                                return `${word[0]}${word[1]}${word[2]}`
+                            } else return `${word[0]}${word[1]}`
+                        } else return `${word[0]}`
+                    } else return ''
+                }
+            }
+            // pushes each word into sorted array in numerical order
+            sorted[pos(word)-1] = word
+        }) 
+        //returns sorted array with numbers
+        return sorted
+    }
+
+    let primeSort = trueSort(arr)
+
+    //loop that ejects 1 element from primeSorted to decodedArr then increases the number of ejected elements until all elements ejected
+    let j = 1
+    while(primeSort[0]){
+        decodedArr.push(primeSort.splice(0,j).pop().slice(2))
+        j++
+    }
+    const sani = decodedArr.join(' ')
+    //sanitizes any loose numbers, preserving order and returns string
+    return sani.split(',').join(' ').replaceAll(/\d/g,'')
 }
 
-Decode(message)
-
-
-//Not quite there yet with methods and all required to make this function respond to an array with any number of elements.
+console.log(decode2(bigMessage))
